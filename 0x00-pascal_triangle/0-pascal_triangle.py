341b073dcd7d766b1d10539bb8409b3b4465da35
kaytee07 <kofi.taylor07@gmail.com>
2023-09-26 16:02:39 +0000

    add triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,8 +12,7 @@ def get_pascal(num, arr):
         return arr
     if (len(arr[-1]) == 1):
         arr.append([1, 1])
-        num -= 1
-        return get_pascal(num, arr)
+        return get_pascal(num - 1, arr)
     newarr = []
     first = 0
     second = 1
@@ -23,9 +22,8 @@ def get_pascal(num, arr):
         first += 1
         second += 1
     newarr.append(arr[-1][-1])
-    num -= 1
     arr.append(newarr)
-    return get_pascal(num, arr)
+    return get_pascal(num - 1, arr)
 
 
 def pascal_triangle(n):
@@ -33,7 +31,7 @@ def pascal_triangle(n):
     returns a list of lists of integers representing the Pascal’s
     triangle of n
     """
-    if n <= 0:
+    if n <= 0 or type(n) is not int:
         return [[]]
     n -= 1
     return get_pascal(n, [[1]])
