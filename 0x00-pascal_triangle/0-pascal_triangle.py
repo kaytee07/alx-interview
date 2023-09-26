4016cffb99f7d4c1957a5dfc4b68bc026b8bb019
kaytee07 <kofi.taylor07@gmail.com>
2023-09-26 12:47:50 +0000

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+pascals triangle
+"""
+
+
+def get_pascal(num, arr):
+    """
+    recurse
+    """
+    if (num == 0):
+        return arr
+    if (len(arr[-1]) == 1):
+        arr.append([1, 1])
+        num -= 1
+        return get_pascal(num, arr)
+    newarr = []
+    first = 0
+    second = 1
+    newarr.append(arr[-1][0])
+    while (second < len(arr[-1])):
+        newarr.append(arr[-1][first] + arr[-1][second])
+        first += 1
+        second += 1
+    newarr.append(arr[-1][-1])
+    num -= 1
+    arr.append(newarr)
+    return get_pascal(num, arr)
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s
+    triangle of n
+    """
+    if n <= 0:
+        return [[]]
+    n -= 1
+    return get_pascal(n, [[1]])
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+## PASCAL'S TRIANGLE
+Pascal's triangle is the triangular array of numbers that begins with 1 on the top and with 1's running down the two sides of a triangle
