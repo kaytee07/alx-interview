631113986881cd50f6f12d84310722f1a234df9c
kaytee07 <kofi.taylor07@gmail.com>
2023-10-11 23:10:33 +0000

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env/ python3
+"""
+min number of operations to print the H character n times
+"""
+
+
+def minoperations(n):
+    """
+    get the number of operations to print the H character
+
+    Args:
+        n is the number of times to print the H character
+
+    Return:
+        the minimum number of operations
+    """
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
