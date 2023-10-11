71ced210c9b5c9f514ca89aa961dae427fcd631a
kaytee07 <kofi.taylor07@gmail.com>
2023-10-11 23:24:42 +0000

    add

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,21 +14,15 @@ def minoperations(n):
     Return:
         the minimum number of operations
     """
-    if not isinstance(n, int):
+    if n == 1:
         return 0
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    while done < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif clipboard > 0:
-            done += clipboard
-            ops_count += 1
-    return ops_count
+    
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + dp[i // j] + 1)
+
+    return dp[n] if dp[n] != float('inf') else 0
