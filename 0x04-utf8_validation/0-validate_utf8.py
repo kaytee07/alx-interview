1f0decc8b8da26b03c45d1baacdb4ae29b08fb0e
kaytee07 <kofi.taylor07@gmail.com>
2023-10-26 07:30:00 +0000

    pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,27 +12,20 @@ def validUTF8(data):
     trailing_bytes = 0
 
     for byte in data:
-        # Check if it's a start byte
         if trailing_bytes == 0:
-            # 1-byte character
             if (byte & 0b10000000) == 0:
                 continue
-            # 2-byte character
             elif (byte & 0b11100000) == 0b11000000:
                 trailing_bytes = 1
-            # 3-byte character
             elif (byte & 0b11110000) == 0b11100000:
                 trailing_bytes = 2
-            # 4-byte character
             elif (byte & 0b11111000) == 0b11110000:
                 trailing_bytes = 3
             else:
                 return False
         else:
-            # Check if it's a trailing byte
             if (byte & 0b11000000) != 0b10000000:
                 return False
             trailing_bytes -= 1
 
-    # Ensure all expected trailing bytes were present
     return trailing_bytes == 0
