011f54df0c48166ffc4a7892741f8660cef2e8cd
kaytee07 <kofi.taylor07@gmail.com>
2023-11-23 12:58:32 +0000

    executable

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,17 +7,19 @@ def makeChange(coins, total):
     """
     make change
     """
-    if total < 0:
+    if total <= 0:
         return 0
-    if total == 0:
-        return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
